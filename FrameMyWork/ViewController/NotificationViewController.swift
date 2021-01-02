1e91b9ee15d36b4c88abed135b6c8b5bf491d785
equalnbgd <40184117+equalnbgd@users.noreply.github.com>
2021-01-02 15:13:33 +0100

    photographer storyboard v3.0.0

---
+++ FrameMyWork/ViewController/NotificationViewController.swift
@@ -0,0 +1,23 @@
+//
+//  NotificationViewController.swift
+//  FrameMyWork
+//
+//  Created by Marko Bozilovic on 2.1.21..
+//
+
+import UIKit
+
+class NotificationViewController: UIViewController {
+    // MARK: - Outlets
+    
+    @IBOutlet var emptyStateImageView: UIImageView!
+    @IBOutlet var emptyStateLabel: UILabel!
+
+    // MARK: - LifeCycleFunctions
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
