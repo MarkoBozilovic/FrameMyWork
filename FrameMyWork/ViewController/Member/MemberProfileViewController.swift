0fdcf23b0c8f64d866560c55469eda35bedd2454
Stefana <stfts@icloud.com>
2021-01-08 23:07:06 +0100

    Member profile storyboard

--- FrameMyWork/ViewController/Member/MemberProfileViewController.swift
+++ FrameMyWork/ViewController/Member/MemberProfileViewController.swift
@@ -7,23 +7,55 @@
 
 import UIKit
 
-class MemberProfileViewController: UIViewController {
+class MemberProfileViewController: UIViewController, UITextViewDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet var profileImage: UIImageView!
+    @IBOutlet var aboutTextView: UITextView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        self.aboutTextView.delegate = self
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //MARK: - Actions
+    
+    @IBAction func editImagePressed() {
+        let vc = UIImagePickerController()
+        vc.sourceType = .photoLibrary
+        vc.delegate = self
+        vc.allowsEditing = true
+        present(vc, animated: true)
     }
-    */
+    
+    //MARK: - Functions
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        self.aboutTextView.resignFirstResponder()
+    }
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        self.aboutTextView.backgroundColor = UIColor.lightGray
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        self.aboutTextView.backgroundColor = UIColor.white
+    }
+    
+    //UIImagePickerControllerDelegate
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let image = info[UIImagePickerController.InfoKey(rawValue: "UIImagePickerControllerEditedImage")] as? UIImage {
+            profileImage.image = image
+        }
+        
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
 
 }
