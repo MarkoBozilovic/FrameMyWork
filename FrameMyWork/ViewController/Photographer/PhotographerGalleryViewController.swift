baedaa56a3360de000e43520965f506b49458bce
Stefana <stfts@icloud.com>
2021-01-21 22:08:13 +0100

    Photographer profile

---
+++ FrameMyWork/ViewController/Photographer/PhotographerGalleryViewController.swift
@@ -0,0 +1,28 @@
+//
+//  PhotographerGalleryViewController.swift
+//  FrameMyWork
+//
+//  Created by Stefana on 20/01/2021.
+//
+
+import UIKit
+
+class PhotographerGalleryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- FrameMyWork/ViewController/Photographer/PhotographerProfileViewController.swift
+++ FrameMyWork/ViewController/Photographer/PhotographerProfileViewController.swift
@@ -7,12 +7,59 @@
 
 import UIKit
 
-class PhotographerProfileViewController: UIViewController {
-
+class PhotographerProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    //MARK: - Properties
+//    let gallery = PhotographerGalleryViewController()
+//    let review = PhotographerReviewViewController()
+    
+    //MARK: - Outlets
+    @IBOutlet var profileImage: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var aboutTextView: UITextView!
+    @IBOutlet var galleryReviewSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var galleryContainer: UIView!
+    @IBOutlet weak var reviewContainer: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        nameLabel.text = Model.shared.user!.profile!.firstName + " " + Model.shared.user!.profile!.lastName
         // Do any additional setup after loading the view.
+    }
+    
+    //MARK: - Actions
+    @IBAction func editImagePressed() {
+        let vc = UIImagePickerController()
+        vc.sourceType = .photoLibrary
+        vc.delegate = self
+        vc.allowsEditing = true
+        present(vc, animated: true)
+    }
+    
+    @IBAction func didTapSegment(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            self.galleryContainer.alpha = 1
+            self.reviewContainer.alpha = 0
+        } else {
+            self.galleryContainer.alpha = 0
+            self.reviewContainer.alpha = 1
+        }
+    }
+    
+    //MARK: - UIImagePickerControllerDelegate
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let image = info[UIImagePickerController.InfoKey(rawValue: "UIImagePickerControllerEditedImage")] as? UIImage {
+            profileImage.image = image
+        }
+        
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
     }
     
 }
